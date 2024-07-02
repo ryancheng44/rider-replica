7ea1af25a09e4b2c099113809b0f61a44605c582
ryancheng44 <ryancheng44@gmail.com>
2024-07-02 12:16:46 -0400

    Added Rider Replica project

---
+++ Rider Replica/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = target.position + offset;
+        newPosition.z = transform.position.z;
+
+        transform.position = newPosition;
+    }
+}
